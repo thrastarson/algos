99e2315ad43f42c5b737dbe04e41bfd390f2e6f7
Arni Thrastarson <arnmtra@gmail.com>
2018-01-30 20:23:09 -0800

    Add implementation of insertion sort.

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+from utils import test_sorting, switch
+
+def insertion_sort(a):
+    i = 0
+    while i < len(a) - 1:
+        j = i + 1
+        while j > 0 and a[j] < a[j-1]:
+            switch(a, j-1, j)
+            j -= 1
+        i += 1
+
+def main():
+    test_sorting(insertion_sort, returns_sorted=False)
+
+if __name__ == '__main__':
+    main()
