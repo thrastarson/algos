c07b31e8003e2093ae56b9b172ca2b429aeab54b
Arni Thrastarson <arnmtra@gmail.com>
2018-01-30 21:39:35 -0800

    Add implementation of selection sort.

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+from utils import switch, test_sorting
+
+def selection_sort(a):
+    i = len(a) - 1
+    while i > 0:
+        j = i - 1
+        while j >= 0:
+            if a[j] > a[i]:
+                switch(a, j, i)
+            j -= 1
+        i -= 1
+
+def main():
+    test_sorting(selection_sort, returns_sorted=False)
+
+if __name__ == '__main__':
+    main()
