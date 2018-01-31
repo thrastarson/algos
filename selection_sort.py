b778af3ca7e7d8bab88c2e238eaa6e6c6d4a7ba8
Arni Thrastarson <arnmtra@gmail.com>
2018-01-30 23:15:52 -0800

    Fix implementation of selection sort to use the correct algorithm.

--- selection_sort.py
+++ selection_sort.py
@@ -1,14 +1,14 @@
 from utils import swap, test_sorting
 
 def selection_sort(a):
-    i = len(a) - 1
-    while i > 0:
-        j = i - 1
-        while j >= 0:
-            if a[j] > a[i]:
-                swap(a, j, i)
-            j -= 1
-        i -= 1
+    for i in range(len(a)):
+        min_index = i
+        for j in range(i+1, len(a)):
+            if a[j] < a[min_index]:
+                min_index = j
+        
+        if min_index != i:
+            swap(a, min_index, i)
 
 def main():
     test_sorting(selection_sort, returns_sorted=False)
