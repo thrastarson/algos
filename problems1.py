672b39d439619fa102d17c472c0fdc591167288c
Arni Thrastarson <arnmtra@gmail.com>
2018-02-06 10:39:36 -0800

    Add solution to problem 1.9.

--- problems1.py
+++ problems1.py
@@ -295,20 +295,11 @@ def zero_matrix(matrix):
 
     return True
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Problem 1.9
+#Assume that you have a method isSubstring which checks if one word is a substring
+#of another. Given two strings, s1 and s2, write code to check if s2 is a rotation
+#of s1 using only one call to isSubstring (e.g. "waterbottle" is a rotation
+#of "erbottlewat".
+def string_rotation(s1, s2):
+    s1s1 = s1 + s1
+    return s2 in s1s1
