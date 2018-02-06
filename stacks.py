8c90b996b8e508b004bb76df6347e7beaa1d4918
Arni Thrastarson <arnmtra@gmail.com>
2018-02-05 18:25:45 -0800

    Add 3 implementation of the stack data structure. Stack is a simple pythonic implementation, ArrayStack pretends to be a fixed length array implementation, and ListStack is an implementation that uses a linked list.

---
+++ stacks.py
@@ -0,0 +1,81 @@
+from lists import LinkedList
+from utils import get_random_list
+
+class Stack:
+    def __init__(self):
+        self._list = []
+
+    def push(self, data):
+        self._list.append(data)
+
+    def pop(self):
+        if not self.is_empty():
+            return self._list.pop()
+
+    def is_empty(self):
+        return len(self._list) == 0
+
+    def print_stack(self):
+        print(self._list)
+
+class ArrayStack(Stack):
+    def __init__(self, size=20):
+        self._list = []
+        self.count = 0
+        self.size = size
+
+    def push(self, data):
+        if self.count >= self.size:
+            #Pretend self._list is a fixed size array.
+            #Double the size of the array to increase
+            #the size of the stack.
+            self.size = self.size * 2
+            bigger_list = [x for x in self._list]
+            self._list = bigger_list
+        
+        self._list.append(data)
+        self.count += 1
+
+    def pop(self):
+        if not self.is_empty():
+            self.count -= 1
+            return self._list[self.count]
+
+    def is_empty(self):
+        return self.count == 0
+
+class ListStack(Stack):
+    def __init__(self):
+        self._list = LinkedList()
+
+    def push(self, data):
+        self._list.insert(data)
+
+    def pop(self):
+        if not self.is_empty():
+            head = self._list.get_first()             
+            return head.data
+
+    def is_empty(self):
+        return self._list.is_empty()
+
+    def print_stack(self):
+        self._list.print_list()
+
+def main():
+    a = get_random_list()
+    print(a)
+
+    stack_classes = (Stack, ArrayStack, ListStack)
+    for stack_class in stack_classes:
+        stack = stack_class()
+        for x in a:
+            stack.push(x)
+        stack.print_stack()
+
+        while not stack.is_empty():
+            print('Popping %s..' % stack.pop())
+        stack.print_stack()
+
+if __name__ == '__main__':
+    main()
