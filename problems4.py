dd2fc76ef9d0ad5aa20fc23936af0204ec7b2b6f
Arni Thrastarson <arnmtra@gmail.com>
2018-02-11 18:22:11 -0800

    Add solution to problem 4.2.

--- problems4.py
+++ problems4.py
@@ -1,4 +1,5 @@
 from graphs import bf_search, get_sample_graph
+from trees import BinaryTreeNode, bst_insert
 
 #Problem 4.1
 #Given a directed graph, design an algorithm to find out whether there is
@@ -24,3 +25,30 @@ def test_route():
     print('s: %s' % s)
     print('t: %s' % t)
     route_between_two_nodes(s, t)
+
+#Problem 4.2
+#Given a sorted (increasing order) array with unique integer elements,
+#write an algorithm to create a binary search tree with minimal height.
+def minimal_tree():
+    a = [1, 2, 3, 4, 5]
+    m = len(a) // 2
+    
+    root = BinaryTreeNode(a[m])
+    i = m - 1
+    j = m + 1
+    while i >= 0 or j < len(a):
+        left_tree_node = BinaryTreeNode(a[i])
+        bst_insert(root, left_tree_node)
+        i += 1
+
+        right_tree_node = BinaryTreeNode(a[j])
+        bst_insert(root, right_tree_node)
+        j -= 1
+
+    while i >= 0:
+        left_tree_node = BinaryTreeNode(a[i])
+        bst_insert(root, left_tree_node)
+
+    while j < len(a):
+        right_tree_node = BinaryTreeNode(a[j])
+        bst_insert(root, right_tree_node)
