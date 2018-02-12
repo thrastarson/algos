98b0e20d81664f0fdb479e45727e715d2aa423db
Arni Thrastarson <arnmtra@gmail.com>
2018-02-11 16:35:50 -0800

    Add some handy functions to GraphNode and Graph. Factor GraphNode out of the Graph class. Add test to create a small random graph.

--- graphs.py
+++ graphs.py
@@ -1,18 +1,39 @@
+import random
 from queues import MyQueue
+from utils import get_random_list
+
+class GraphNode:
+    #Implements a graph node with an adjacency list.
+    def __init__(self, val=None, adjacent=None):
+        self.val = val
+        if adjacent is None:
+            self.adjacent = []
+        else:
+            self.adjacent = adjacent
+        self.marked = False
+
+    def add_edge(self, node):
+        self.adjacent.append(node)
+
+    def __str__(self):
+        return str(self.val)
 
 class Graph:
     #Implements a graph using adjacency lists.
-    class GraphNode:
-        def __init__(self, name=None, adjacent=None):
-            self.name = name
-            if adjacent is None:
-                self.adjacent = []
-            else:
-                self.adjacent = adjacent
-            self.marked = False
-
     def __init__(self):
         self.nodes = []
+
+    def add(self, node):
+        self.nodes.append(node)
+
+    def get_nodes(self):
+        return self.nodes
+
+    def size(self):
+        return len(self.nodes)
+
+    def __string__(self):
+        return str(self.nodes)
 
 def df_search(root, visit):
     visit(root)
@@ -41,4 +62,21 @@ class MatrixGraph:
     def __init__(self, size=10):
         self.size = size
         self.matrix = [[False for x in range(n)] for y in range(n)]
-        
+
+
+def main():
+    a = get_random_list()
+    
+    g = Graph()
+    for x in a:
+        node = GraphNode(val=x)
+        g.add(node)
+
+    nodes = g.get_nodes()
+    for node in nodes:
+        #Add two random directed edges for each node.
+        node.add_edge(random.choice(nodes))
+        node.add_edge(random.choice(nodes))
+
+if __name__ == '__main__':
+    main()
