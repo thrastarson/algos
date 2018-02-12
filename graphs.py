3d282b0a64bfd8672f3de4ffabd2e30700234b2a
Arni Thrastarson <arnmtra@gmail.com>
2018-02-11 16:09:24 -0800

    Add implementation of depth-first and breadth-first searches. Change variable names in Graph.

--- graphs.py
+++ graphs.py
@@ -1,15 +1,40 @@
+from queues import MyQueue
+
 class Graph:
     #Implements a graph using adjacency lists.
     class GraphNode:
-        def __init__(self, name=None, children=None):
+        def __init__(self, name=None, adjacent=None):
             self.name = name
-            if children is None:
-                self.children = []
+            if adjacent is None:
+                self.adjacent = []
             else:
-                self.children = children
+                self.adjacent = adjacent
+            self.marked = False
 
     def __init__(self):
         self.nodes = []
+
+def df_search(root, visit):
+    visit(root)
+    root.marked = True
+    for node in root.adjacent:
+        if node.marked is False:
+            df_search(node, visit)
+
+def bf_search(root, visit):
+    q = MyQueue()
+    q.add(root)
+    
+    while not q.is_empty():
+        node = q.remove()
+        node.marked = True
+        visit(node)
+        
+        for adjacent_node in node.adjacent:
+            if adjacent_node.marked is False:
+                adjacent_node.marked = True
+                q.add(adjacent_node)
+
 
 class MatrixGraph:
     #Implements a graph using an adjacency matrix.
