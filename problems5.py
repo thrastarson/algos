c6a795eee05b960b57425d7a226821f4e72abe7f
Arni Thrastarson <arnmtra@gmail.com>
2018-02-18 16:23:33 -0800

    Add solution to problem 5.8.

--- problems5.py
+++ problems5.py
@@ -187,51 +187,17 @@ def pairwise_swap(n):
     
     return swapped
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Problem 5.8
+#A monochrome screen is stored as a single array of bytes, allowing eight
+#consecutive pixels to be stored in one byte. The screen has width w,
+#where w is divisible by 8 (that is, no byte will be split across rows).
+#The height of the screen, of course, can be derived from the length
+#of the array and the width. Implement a function that draws a horizontal
+#line from (x1, y) to (x2, y).
+#The method signature should look something like:
+#drawLine(byte[] screen, int width, int x1, int x2, int y)
+def draw_line(screen, width, x1, x2, y):
+    rows = screen // width
+    for i in range(y * 8 + x1: y * 8 + x2 + 1):
+        screen[i] = 1
+    return screen
