1a275d376cf72df2ec4769f8325ac642e13e1e9b
Arni Thrastarson <arnmtra@gmail.com>
2018-02-19 20:06:17 -0800

    Add solution to problem 6.4.

--- problems6.py
+++ problems6.py
@@ -75,4 +75,32 @@ Solution:
     The task is impossible.
 """
 
+#Problem 6.4
+#There are three ants on different vertices of a triangle. What is the
+#probability of collision (between any two or all of them) if they start
+#walking on the sides of the triangle? Assume that each ant randomly picks
+#a direction, with either direction being equally likely to be chosen,
+#and that they walk at the same speed.
+#Similarly, find the probability of collision with n ants on an n-vertex polygon.
+"""
+Solution:
+    Let's consider the event of no collision happening.
+    This can only happen if all ants choose to walk in the same direction.
+    The ants can choose either direction and avoid collision, so
 
+    P(no collision) = P(Ar)*P(Br)*P(Cr) + P(Al)*P(Bl)*P(Cr)
+
+    where the ants are labelled A, B, and C, and "Ar" is the event
+    of ant A walking to the right.
+    All individual probabilities are the same, so we get
+
+    P(no collision) = (1/2)^3 + (1/2)^3
+                    = 0.125 + 0.125
+                    = 0.25
+
+    So we arrive at
+
+    P(collision) = 1 - P(no collision)
+                 = 1 - 0.25
+                 = 0.75
+"""
