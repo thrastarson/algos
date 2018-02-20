21a556b0c360b58319773cf42946b50357a4d08c
Arni Thrastarson <arnmtra@gmail.com>
2018-02-19 17:21:39 -0800

    Add solution to problem 6.2.

--- problems6.py
+++ problems6.py
@@ -15,3 +15,42 @@ Solution:
     weight of the overweight 1.1g pills on the scale.
     Calculate I = 10 * R to find the index I of the bottle with the heavy pills.
 """
+
+#Problem 6.2
+#You have a basketball hoop and someone says that you can play one
+#of two games.
+#Game 1: You get one shot to make the hoop.
+#Game 2: You get three shots and you have to make two of three shots.
+#If p is the probability of making a particular shot, for which values of p
+#should you pick one game or the other?
+"""
+Solution:
+    We must construct when P(G1) > P(G2).
+    P(G1) is simply p.
+    P(G2) is the probability of making at least 2 shots out of 3.
+    The probability of making exactly 2 shots is p^2*(1-p).
+    The probability of making all 3 shots is p^3.
+    There are exactly 3 possible ways of making exactly 2 shots out of 3:
+    (++-), (+-+), and (-++).
+    So P(G2) = 3 * p^2*(1-p) + p^3.
+    We therefore choose G1 when p > 3 * p^2*(1-p) + p^3.
+    => p > 3p^2 - 3p^3 + p^3
+    => p > 3p^2 - 2p^3
+    => 1 > 3p - 2p^2
+    => 2p^2 - 3p + 1 > 0
+    => (2p-1)(p-1) > 0
+
+    For the inequality to hold, both terms on the left side need to be
+    positive, or obth terms need to be positive.
+    We also know the constraint that 0 < p < 1, so the term (p-1) must
+    always be negative.
+    For the first term to be negative also we need,
+
+    2p-1 < 0
+    => 2p < 1
+    => p < 1/2
+
+    So we should play G1 if 0 < p < 1/2, and G2 if not.
+    Most people will should a basketball with p < 1/2, so realistically
+    choosing G1 is most often the best choice.
+"""
