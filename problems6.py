cc40feb5a6832aaafacd0d1467b16d50f26d3293
Arni Thrastarson <arnmtra@gmail.com>
2018-02-19 17:22:22 -0800

    Add attribution to Gayle as the problem author.

--- problems6.py
+++ problems6.py
@@ -1,3 +1,6 @@
+#The following problems are from the book Cracking the Coding Interview
+#by Gayle Laakmann McDowell. I reserve no rights for them.
+
 #Problem 6.1
 #You have 20 bottles of pills. 19 bottles have 1.0 gram pills,
 #but one has pills of weight 1.1 grams. Given a scale that provides
