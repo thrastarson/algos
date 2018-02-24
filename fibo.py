621169578dd90e8c381cd8969712524315db08d0
Arni Thrastarson <arnmtra@gmail.com>
2018-02-23 18:24:51 -0800

    Add dynamic programming solution to fibo().

--- fibo.py
+++ fibo.py
@@ -5,3 +5,19 @@ def fibonacci(i):
         return 1
     else:
         return fibonacci(i-1) + fibonacci(i-2)
+
+def fibo(i):
+    results = {}
+    fibo_dp(i, results)
+
+def fibo_dp(i, results):
+    if i == 0:
+        return 0
+    elif i == 1:
+        return 1
+    else:
+        try:
+            return results[i]
+        except KeyError:
+            results[i] = fibo_dp(i - 1, results) + fibo_dp(i - 2, results)
+            return results[i]
