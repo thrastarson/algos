b1ccc48df0e8a2148242b52025bb74d0243123c5
Arni Thrastarson <arnmtra@gmail.com>
2018-02-23 18:28:31 -0800

    Add iterative fibo solution that builds up a sequence.

--- fibo.py
+++ fibo.py
@@ -21,3 +21,17 @@ def fibo_dp(i, results):
         except KeyError:
             results[i] = fibo_dp(i - 1, results) + fibo_dp(i - 2, results)
             return results[i]
+
+def fibo_up(i):
+    if i == 0 or i == 1:
+        return i
+
+    a = 0
+    b = 1
+    c = 0
+    for k in range(2, i + 1):
+        c = a + b
+        a = b
+        b = c
+
+    return c
