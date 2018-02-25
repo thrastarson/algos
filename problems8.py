11c6f83ce12fce91cb88d586b017025853fa93f4
Arni Thrastarson <arnmtra@gmail.com>
2018-02-24 17:54:13 -0800

    Improve base case for solutions in problem 8.1.

--- problems8.py
+++ problems8.py
@@ -8,12 +8,10 @@ def triple_step(n):
     This is a simple recursive solution but it is very
     inefficient, O(3^n).
     """
-    if n == 1:
+    if n < 0:
+        return 0
+    elif n == 0:
         return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
     else:
         triple_step(n-1) + triple_step(n-2) + triple_step(n-3)
 
@@ -24,16 +22,14 @@ def triple_step2(n, memo=None):
     """
     if memo is None:
         memo = {}
-        memo[1] = 1
-        memo[2] = 2
-        memo[3] = 4
+        memo[0] = 1
 
-    if n == 1 or n == 2 or n == 3:
+    if n < 0:
+        return 0
+    
+    try:
         return memo[n]
-    else:
-        try:
-            return memo[n]
-        except KeyError:
-            memo[n] = (triple_step2(n-1, memo) + triple_step2(n-2, memo)
-                       triple_step2(n-3, memo))
-            return memo[n]
+    except KeyError:
+        memo[n] = (triple_step2(n-1, memo) + triple_step2(n-2, memo)
+                   triple_step2(n-3, memo))
+        return memo[n]
