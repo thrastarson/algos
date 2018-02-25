2e5ed81fb0be36ba39f90ed49c8b51d2b9ff8de2
Arni Thrastarson <arnmtra@gmail.com>
2018-02-24 18:39:47 -0800

    Add solution to problem 8.2.

--- problems8.py
+++ problems8.py
@@ -33,3 +33,31 @@ def triple_step2(n, memo=None):
         memo[n] = (triple_step2(n-1, memo) + triple_step2(n-2, memo)
                    triple_step2(n-3, memo))
         return memo[n]
+
+#Problem 8.2
+#Imagine a robot sitting on the upper left corner of grid with r rows
+#and c columns. The robot can only move in two directions, right and down,
+#but certain cells are "off limits" such that the robot cannot step
+#on them. Design an algorithm to find a path for the robot from the top
+#left to the bottom right.
+def find_path(grid, row=None, col=None, path=None):
+    if grid is None or len(grid) == 0:
+        return None
+
+    if path is None:
+        path = []
+
+    if row is None:
+        row = len(grid)
+
+    if col is None:
+        col = len(grid[0])
+
+    at_end_point = col == 0 and row == 0
+    if (at_end_point 
+        or find_path(grid, row-1, col, path) 
+        or find_path(grid, row, col-1, path)):
+        path.append((col, row))
+        return True
+
+    return False
