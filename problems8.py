1a0abf8f8c59fcf7660f3bb6ab0089e6bbd06095
Arni Thrastarson <arnmtra@gmail.com>
2018-02-24 17:44:49 -0800

    Add a more efficient solution to problem 8.1.

--- problems8.py
+++ problems8.py
@@ -4,6 +4,10 @@
 #a method to count how many possible ways the child can run up
 #the stairs.
 def triple_step(n):
+    """
+    This is a simple recursive solution but it is very
+    inefficient, O(3^n).
+    """
     if n == 1:
         return 1
     elif n == 2:
@@ -13,3 +17,23 @@ def triple_step(n):
     else:
         triple_step(n-1) + triple_step(n-2) + triple_step(n-3)
 
+def triple_step2(n, memo=None):
+    """
+    This modification uses memoization to bring the efficiency
+    up to O(n).
+    """
+    if memo is None:
+        memo = {}
+        memo[1] = 1
+        memo[2] = 2
+        memo[3] = 4
+
+    if n == 1 or n == 2 or n == 3:
+        return memo[n]
+    else:
+        try:
+            return memo[n]
+        except KeyError:
+            memo[n] = (triple_step2(n-1, memo) + triple_step2(n-2, memo)
+                       triple_step2(n-3, memo))
+            return memo[n]
