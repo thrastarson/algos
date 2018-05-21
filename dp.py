9eb42b40ea7011238ee16cc3c49fc2472b03e648
thrastarson <arnmtra@gmail.com>
2018-05-20 18:36:23 -0700

    Add solution to the Longest Increasing Subsequence problem.

--- dp.py
+++ dp.py
@@ -1,5 +1,5 @@
 """
-Longest increasing subsequence
+Longest increasing subsequence by one
 Given N elements, write a program that prints the length of the longest increasing
 subsequence whose adjacent element difference is one.
 
@@ -8,7 +8,7 @@ Output : 6
 Explanation: 3, 4, 5, 6, 7, 8 is the longest increasing subsequence 
 whose adjacent element differs by one.
 """
-def longest_increasing_subsequence(a):
+def longest_increasing_subsequence_by_one(a):
     """
     Solution:
     memo[i] stores the length of the longest subsequence which ends with a[i].
@@ -16,12 +16,39 @@ def longest_increasing_subsequence(a):
     then a[i] will add to the increasing subsequence which has a[i] - 1.
     """
     index_of = {}
-    memo = [0 for x in a]
+    memo = [0 for _ in a]
     for i, el in enumerate(a):
         index_of[el] = i
         try:
             memo[i] = memo[index_of[el - 1]] + 1
         except KeyError:
+            memo[i] = 1
+
+    return max(memo)
+
+"""
+The Longest Increasing Subsequence (LIS) problem is to find the length 
+of the longest subsequence of a given sequence such that all elements 
+of the subsequence are sorted in increasing order. 
+For example, the length of LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} 
+is 6 and LIS is {10, 22, 33, 50, 60, 80}.
+"""
+def longest_increasing_subsequence(a):
+    """
+    Solution:
+    Let arr[0..n-1] be the input array and L(i) be the length of the LIS 
+    ending at index i such that arr[i] is the last element of the LIS.
+    Then, L(i) can be recursively written as:
+        L(i) = 1 + max( L(j) ) where 0 < j < i and arr[j] < arr[i]; or
+        L(i) = 1, if no such j exists.
+    To find the LIS for a given array, we need to return max(L(i)) where 0 < i < n.
+    """
+    memo = [0 for _ in a]
+    for i in range(len(a)):
+        prefix_path = [path for j, path in enumerate(memo[:i]) if a[j] < a[i]]
+        if len(prefix_sequence_lengths) > 0:
+            memo[i] = max(prefix_sequence_lengths) + 1
+        else:
             memo[i] = 1
 
     return max(memo)
@@ -48,15 +75,20 @@ def prefix_sum_of_matrix(a):
             elif j == 0:
                 psa[i][j] = a[i][j] + psa[i-1][j]
             else:
+                #The tricky part is to remember to subtract psa[i-1][j-1]
+                #so as not to double count.
                 psa[i][j] = a[i][j] + psa[i-1][j] + psa[i][j-1] - psa[i-1][j-1]
 
     return psa
 
 if __name__ == '__main__':
     a = [3, 10, 3, 11, 4, 5, 6, 7, 8, 12]
-    print(longest_increasing_subsequence(a))
+    print(longest_increasing_subsequence_by_one(a))
 
     a = [[10, 20, 30],
          [5, 10, 20],
          [2, 4, 6]]
     print(prefix_sum_of_matrix(a))
+
+    a = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+    print(longest_increasing_subsequence(a))
